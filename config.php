0719013505b2b4facee1b910473dd187367e8e74
dwikinuwangsa09 <dwikinuwangsa@9inch.studio>
2023-12-21 08:22:20 +0700

    First Initialize | Beta 0.1

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ session_start();
 // init configuration
 $clientID = '1089634697014-qn74nthln8ev5fbfb33o7223kqv33s69.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'http://localhost/my.9inch/dashboard.php';
+$redirectUri = 'https://my.9inch.broadcast/dashboard.php';
 
 // create Client Request to access Google API
 $client = new Google_Client();
@@ -19,8 +19,8 @@ $client->addScope("profile");
 
 // Connect to database
 $hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "datasembilan";
+$username = "dxklgwhp_datasembilan";
+$password = "9InchBroadcast";
+$database = "dxklgwhp_datasembilan";
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
