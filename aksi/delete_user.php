5ad8a21f770d03be58b142c1770831a4c080da3a
dwikinuwangsa09 <dwikinuwangsa@9inch.studio>
2024-01-20 13:56:27 +0700

    Update delete_user.php

--- aksi/delete_user.php
+++ aksi/delete_user.php
@@ -2,13 +2,13 @@
 // koneksi database
 include '../config/koneksi.php';
  
-$nim   = $_GET['nim'];
+$id   = $_GET['id'];
 
 // query SQL untuk insert data
-$query="DELETE from crew where nim='$nim'";
+$query="DELETE from users where id='$id'";
 mysqli_query($db, $query);
 
 // mengalihkan halaman kembali ke verifikasi.php
-header("location:../crewmanage.php?pesan=berhasilhapus");
+header("location:../user.php?pesan=berhasilhapus");
  
 ?>
